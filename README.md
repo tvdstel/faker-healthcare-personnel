5a781f2d91ab6580c79a129df655683b994c783b
Tamara van Klinken <tamara@motivo.nl>
2022-12-15 14:22:43 +0100

    Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# php-faker-healthcare
-Custom FakerPHP provider for faking teams, location, functions, specialisations etc. in healthcare.
+# faker-healthcare-personnel
+Custom FakerPHP provider for faking teams, locations, functions, specialisations etc. in healthcare.
+
+## Installation
+
+Add the library to your composer.json file
+
+ 	composer require tvkvds/faker-healthcare-personnel
+
+## Usage
+
+To use this with Faker, you must add the class to the Faker generator:
+
+
+
+```php
+$faker = \Faker\Factory::create();
+$faker->addProvider(new \HealthCareTeams\Provider\en_US\HealthCareTeams($faker));
+```
+
+
+## Available Generators
+```php
+$faker->location(); 
+$faker->team(); // optional: location name to prepend
+$faker->specialisationGroup(); 
+$faker->specialisation(); 
+$faker->functionGroup();  
+$faker->function(); 
+$faker->contractType(); 
+```
+
+## Available language specific formatters
+
+* en_US
+* nl_NL
+
+
+
+
---
+++ tests/HealthcareTeamsFaker/HealthCareTeamsTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace HealthcareTeamsFaker;
+
+
+use PHPUnit\Framework\TestCase;
+use Faker\Factory;
+
+final class HealthCareTeamsTest extends TestCase
+{
+
+    protected array|false $folder;
+    protected string $prefix = 'HealthcareTeamsFaker\Provider\\';
+    protected string $suffix = '\HealthCareTeams';
+
+    public function setUp(): void
+    {
+        $this->folder = array_diff(scandir('../../src/Provider'), array('..', '.'));
+    }
+
+    public function test_location_returns_string_in_all_languages(): void
+    {
+        foreach ($this->folder as $folder) {
+            $class = $this->prefix . $folder . $this->suffix;
+            $faker = Factory::create();
+            $faker->addProvider(new $class($faker));
+            $this->assertIsString($faker->location());
+        }
+    }
+
+    public function test_team_returns_string_in_all_languages(): void
+    {
+        foreach ($this->folder as $folder) {
+            $class = $this->prefix . $folder . $this->suffix;
+            $faker = Factory::create();
+            $faker->addProvider(new $class($faker));
+            $this->assertIsString($faker->team());
+        }
+    }
+
+    public function test_function_group_returns_string_in_all_languages(): void
+    {
+        foreach ($this->folder as $folder) {
+            $class = $this->prefix . $folder . $this->suffix;
+            $faker = Factory::create();
+            $faker->addProvider(new $class($faker));
+            $this->assertIsString($faker->functionGroup());
+        }
+    }
+
+    public function test_function_returns_string_in_all_languages(): void
+    {
+        foreach ($this->folder as $folder) {
+            $class = $this->prefix . $folder . $this->suffix;
+            $faker = Factory::create();
+            $faker->addProvider(new $class($faker));
+            $this->assertIsString($faker->function());
+        }
+    }
+
+    public function test_specialisation_group_returns_string_in_all_languages(): void
+    {
+        foreach ($this->folder as $folder) {
+            $class = $this->prefix . $folder . $this->suffix;
+            $faker = Factory::create();
+            $faker->addProvider(new $class($faker));
+            $this->assertIsString($faker->specialisationGroup());
+        }
+    }
+
+    public function test_specialisation_returns_string_in_all_languages(): void
+    {
+        foreach ($this->folder as $folder) {
+            $class = $this->prefix . $folder . $this->suffix;
+            $faker = Factory::create();
+            $faker->addProvider(new $class($faker));
+            $this->assertIsString($faker->specialisation());
+        }
+    }
+
+    public function test_contract_type_returns_string_in_all_languages(): void
+    {
+        foreach ($this->folder as $folder) {
+            $class = $this->prefix . $folder . $this->suffix;
+            $faker = Factory::create();
+            $faker->addProvider(new $class($faker));
+            $this->assertIsString($faker->contractType());
+        }
+    }
+}
